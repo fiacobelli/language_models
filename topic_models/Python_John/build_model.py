974f11b41ecfbe93ef00af7c7afd4379569622a8
Francisco Iacobelli <francisco@iacobelli.cl>
2015-04-10 14:15:44 -0500

    several files now

--- topic_models/Python_John/build_model.py
+++ topic_models/Python_John/build_model.py
@@ -133,6 +133,6 @@ def write_counter_b(outfilename,counter):
 
 if __name__=='__main__':
     script, folder, outfilename = argv
-    counts = process_folder(folder,5,".txt",outfilename)
+    counts = process_folder(folder,5,".txt",100,outfilename)
     write_counter_b(outfilename,counts)
 
